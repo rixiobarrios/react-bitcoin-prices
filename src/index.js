1488bff198967f0316f59742789fb9151b410cf9
kchia <kchia87@gmail.com>
2019-10-13 16:27:15 -0700

    make Router the root component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App/App.js";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App/App.js';
+// we need to import the Router component and and place it as the root component of our application
+import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// By making Router the root component of our app, it will pass down several router - specific objects to its child components.Things like current location and url can be accessed or changed. Additionally, in order to use the other routing components provided by React Router, a Router parent component is necessary.
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root')
+);

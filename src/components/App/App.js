b26629c34989c8c584847400c90fb61b7ee11983
kchia <kchia87@gmail.com>
2019-10-13 16:33:43 -0700

    add link and route for homepage

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from "react";
-import Home from "../Home/Home";
-import "./App.css";
+import React, { Component } from 'react';
+import Home from '../Home/Home';
+import './App.css';
+import { Route, Link } from 'react-router-dom';
 
 class App extends Component {
   constructor(props) {
@@ -19,14 +20,18 @@ class App extends Component {
     return (
       <div>
         <nav>
-          <img
-            src="https://en.bitcoin.it/w/images/en/2/29/BC_Logo_.png"
-            alt=""
-          />
-          <h1>Bitcoin prices</h1>
+          {/* Link - a component for setting the URL and providing navigation between different components in our app without triggering a page refresh. It takes a to property, which sets the URL to whatever path is defined within it. Link can also be used inside of any component that is connected to a Route. */}
+          <Link to="/">
+            <img
+              src="https://en.bitcoin.it/w/images/en/2/29/BC_Logo_.png"
+              alt=""
+            />
+            <h1>Bitcoin prices</h1>
+          </Link>
         </nav>
         <main>
-          <Home />
+          {/* Route - a component that renders a specified component (using either render or component) based on the current url (path) we're at. path should probably match a <Link to=""> defined somewhere. */}
+          <Route path="/" component={Home} />
         </main>
       </div>
     );
